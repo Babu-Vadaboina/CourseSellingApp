5b9cd1b0d592a8ff973bca61fce88d87fcb53cba
Babu-Vadaboina <tatababuvadaboina@gmail.com>
2025-12-16 13:48:18 +0530

    added schema layout for the application

---
+++ db.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const objectId = mongoose.Types.objectId;
+
+const userSchema = Schema({
+  email: { type: String, unique: true },
+  password: { type: String },
+  firstName: { type: String },
+  lastName: { type: String },
+});
+const adminSchema = Schema({
+  email: { type: String, unique: true },
+  password: { type: String },
+  firstName: { type: String },
+  lastName: { type: String },
+});
+const courseSchema = Schema({
+  title: { type: String },
+  price: { type: Number },
+  description: { type: String },
+  imageUrl: { type: String },
+  creatorId: { objectId },
+});
+const purchaseSchema = Schema({
+  userId: objectId,
+  courseId: objectId,
+});
+
+const userModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const purchaseModel = mongoose.model("purchase", purchaseSchema);
+
+module.exports = {
+  userModel,
+  adminModel,
+  courseModel,
+  purchaseModel,
+};
