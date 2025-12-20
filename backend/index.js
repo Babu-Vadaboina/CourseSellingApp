b4421a5e0b626cca98114ad84de670a6fad1e760
Babu-Vadaboina <tatababuvadaboina@gmail.com>
2025-12-20 23:08:06 +0530

    BE- configured dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
+require("dotenv").config();
+
 const userRouter = require("./routes/user");
 const adminRouter = require("./routes/admin");
 const app = express();
 
+app.use(express.json());
+
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/admin", adminRouter);
-app.listen(3000);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`server running on port : ${PORT}`);
+});
