9bb96a4c464a08d847e61467a5c4b94fc4773175
Babu-Vadaboina <tatababuvadaboina@gmail.com>
2025-12-20 23:16:09 +0530

    BE- added isolated db logic

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,39 +1,54 @@
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+// const objectId = mongoose.Types.objectId;
+
+// const userSchema = Schema({
+//   email: { type: String, unique: true },
+//   password: { type: String },
+//   firstName: { type: String },
+//   lastName: { type: String },
+// });
+// const adminSchema = Schema({
+//   email: { type: String, unique: true },
+//   password: { type: String },
+//   firstName: { type: String },
+//   lastName: { type: String },
+// });
+// const courseSchema = Schema({
+//   title: { type: String },
+//   price: { type: Number },
+//   description: { type: String },
+//   imageUrl: { type: String },
+//   creatorId: { objectId },
+// });
+// const purchaseSchema = Schema({
+//   userId: objectId,
+//   courseId: objectId,
+// });
+
+// const userModel = mongoose.model("user", userSchema);
+// const adminModel = mongoose.model("admin", adminSchema);
+// const courseModel = mongoose.model("course", courseSchema);
+// const purchaseModel = mongoose.model("purchase", purchaseSchema);
+
+// module.exports = {
+//   userModel,
+//   adminModel,
+//   courseModel,
+//   purchaseModel,
+// };
+
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const objectId = mongoose.Types.objectId;
+const DB_URI = process.env.MONGODB_URL_DEV;
 
-const userSchema = Schema({
-  email: { type: String, unique: true },
-  password: { type: String },
-  firstName: { type: String },
-  lastName: { type: String },
-});
-const adminSchema = Schema({
-  email: { type: String, unique: true },
-  password: { type: String },
-  firstName: { type: String },
-  lastName: { type: String },
-});
-const courseSchema = Schema({
-  title: { type: String },
-  price: { type: Number },
-  description: { type: String },
-  imageUrl: { type: String },
-  creatorId: { objectId },
-});
-const purchaseSchema = Schema({
-  userId: objectId,
-  courseId: objectId,
-});
-
-const userModel = mongoose.model("user", userSchema);
-const adminModel = mongoose.model("admin", adminSchema);
-const courseModel = mongoose.model("course", courseSchema);
-const purchaseModel = mongoose.model("purchase", purchaseSchema);
-
-module.exports = {
-  userModel,
-  adminModel,
-  courseModel,
-  purchaseModel,
+const connectDB = async () => {
+  try {
+    await mongoose.connect(DB_URI);
+    console.log("connected to MongoDB successfully");
+  } catch (err) {
+    console.error("failed to connect to DB : ", err.message);
+    process.exit(1);
+  }
 };
+
+module.exports = connectDB;
