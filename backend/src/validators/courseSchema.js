e2aa5dc01f77fdf2f79522a7fc8e0c65f2a33dea
Babu-Vadaboina <tatababuvadaboina@gmail.com>
2025-12-23 14:11:13 +0530

    BE - added course schema and zod validation

---
+++ backend/src/validators/courseSchema.js
@@ -0,0 +1,9 @@
+const { z } = require("zod");
+const courseSchema = z.object({
+  title: z.string().min(3),
+  description: z.string().min(10),
+  price: z.number().nonnegative(),
+  imageUrl: z.string().url().optional(),
+  published: z.boolean().optional(),
+});
+module.exports = courseSchema;
