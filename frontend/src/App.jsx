cea9d458c7962eb803ba7a0c1c9aab4db5def63f
Babu-Vadaboina <tatababuvadaboina@gmail.com>
2025-12-27 23:17:11 +0530

    FE - Added router functionality & course page to FE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,27 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Courses from "./pages/Courses";
+import CourseDetails from "./pages/CourseDetails";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Purchases from "./pages/Purchases";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold text-blue-600">
-          Tailwind is working 🚀
-        </h1>
-
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Courses />} />
+        <Route path="/course/:id" element={<CourseDetails />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/purchases" element={<Purchases />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/course.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import api from "../api/axios";
+import CourseCard from "../components/CourseCard";
+
+export default function Courses() {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    api
+      .get("/courses")
+      .then((res) => setCourses(res.data.courses))
+      .catch(console.error);
+  }, []);
+
+  return (
+    <div className="p-6 grid grid-cols-1 md:grid-cols-3 gap-6">
+      {courses.map((course) => (
+        <CourseCard key={course._id} course={course} />
+      ))}
+    </div>
+  );
+}
